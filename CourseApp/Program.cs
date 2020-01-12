d603784486502af93a04afa8b5d8e3a396835604
RinOttawa <rinyshka666@gmail.com>
2020-01-12 20:55:51 +0300

    Внесла правки по замечаниям

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,27 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CourseApp
 {
     public class Program
     {
-        public static void Сalculation(double xH, double xK, double dx)
+        public static double Mathhh(double x)
         {
             double y;
+            y = Math.Pow(Math.Pow(Math.Asin(x), 2) + Math.Pow(Math.Cos(x), 4), 3);
+            return y;
+        }
+
+        public static void Сalculation(double xH, double xK, double dx)
+        {
+            int i = 0;
             for (double x = xH; x <= xK; x = x + dx)
             {
-                y = Math.Pow(Math.Pow(Math.Asin(x), 2) + Math.Pow(Math.Cos(x), 4), 3);
-                Console.WriteLine(y + " ");
+                Console.WriteLine("{0} ", Mathhh(x));
+                i++;
             }
         }
 
-        public static void Сalculation2()
+        public static void Сalculation2(List<double> b)
         {
-            double y2;
-            double[] b = { 0.1, 0.35, 0.4, 0.55, 0.6 };
-            for (int i = 0; i < 5; i++)
+            foreach (double i in b)
             {
-                y2 = Math.Pow(Math.Pow(Math.Asin(b[i]), 2) + Math.Pow(Math.Cos(b[i]), 4), 3);
-                Console.WriteLine(y2 + " ");
+               Console.WriteLine("{0} ", Mathhh(i));
             }
         }
 
@@ -30,7 +35,8 @@ namespace CourseApp
             Console.WriteLine("Task A:");
             Сalculation(0.26, 0.66, 0.08);
             Console.WriteLine("Task B:");
-            Сalculation2();
+            List<double> b = new List<double>() { 0.1, 0.35, 0.4, 0.55, 0.6 };
+            Сalculation2(b);
         }
     }
 }
